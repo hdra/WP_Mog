5a64d66ff34702e69feef782f077898e29c53989
Hendra <hendra2392@gmail.com>
2013-02-25 21:06:57 +0800

    Initial commitM

---
+++ functions.php
@@ -0,0 +1,156 @@
+<?php
+/**
+ * _s functions and definitions
+ *
+ * @package _s
+ * @since _s 1.0
+ */
+
+/**
+ * Set the content width based on the theme's design and stylesheet.
+ *
+ * @since _s 1.0
+ */
+if ( ! isset( $content_width ) )
+	$content_width = 640; /* pixels */
+
+/*
+ * Load Jetpack compatibility file.
+ */
+require( get_template_directory() . '/inc/jetpack.php' );
+
+if ( ! function_exists( '_s_setup' ) ) :
+/**
+ * Sets up theme defaults and registers support for various WordPress features.
+ *
+ * Note that this function is hooked into the after_setup_theme hook, which runs
+ * before the init hook. The init hook is too late for some features, such as indicating
+ * support post thumbnails.
+ *
+ * @since _s 1.0
+ */
+function _s_setup() {
+
+	/**
+	 * Custom template tags for this theme.
+	 */
+	require( get_template_directory() . '/inc/template-tags.php' );
+
+	/**
+	 * Custom functions that act independently of the theme templates
+	 */
+	require( get_template_directory() . '/inc/extras.php' );
+
+	/**
+	 * Customizer additions
+	 */
+	require( get_template_directory() . '/inc/customizer.php' );
+
+	/**
+	 * WordPress.com-specific functions and definitions
+	 */
+	//require( get_template_directory() . '/inc/wpcom.php' );
+
+	/**
+	 * Make theme available for translation
+	 * Translations can be filed in the /languages/ directory
+	 * If you're building a theme based on _s, use a find and replace
+	 * to change '_s' to the name of your theme in all the template files
+	 */
+	load_theme_textdomain( '_s', get_template_directory() . '/languages' );
+
+	/**
+	 * Add default posts and comments RSS feed links to head
+	 */
+	add_theme_support( 'automatic-feed-links' );
+
+	/**
+	 * Enable support for Post Thumbnails
+	 */
+	add_theme_support( 'post-thumbnails' );
+
+	/**
+	 * This theme uses wp_nav_menu() in one location.
+	 */
+	register_nav_menus( array(
+		'primary' => __( 'Primary Menu', '_s' ),
+	) );
+
+	/**
+	 * Enable support for Post Formats
+	 */
+	add_theme_support( 'post-formats', array( 'aside', 'image', 'video', 'quote', 'link' ) );
+}
+endif; // _s_setup
+add_action( 'after_setup_theme', '_s_setup' );
+
+/**
+ * Setup the WordPress core custom background feature.
+ *
+ * Use add_theme_support to register support for WordPress 3.4+
+ * as well as provide backward compatibility for WordPress 3.3
+ * using feature detection of wp_get_theme() which was introduced
+ * in WordPress 3.4.
+ *
+ * @todo Remove the 3.3 support when WordPress 3.6 is released.
+ *
+ * Hooks into the after_setup_theme action.
+ */
+function _s_register_custom_background() {
+	$args = array(
+		'default-color' => 'ffffff',
+		'default-image' => '',
+	);
+
+	$args = apply_filters( '_s_custom_background_args', $args );
+
+	if ( function_exists( 'wp_get_theme' ) ) {
+		add_theme_support( 'custom-background', $args );
+	} else {
+		define( 'BACKGROUND_COLOR', $args['default-color'] );
+		if ( ! empty( $args['default-image'] ) )
+			define( 'BACKGROUND_IMAGE', $args['default-image'] );
+		add_custom_background();
+	}
+}
+add_action( 'after_setup_theme', '_s_register_custom_background' );
+
+/**
+ * Register widgetized area and update sidebar with default widgets
+ *
+ * @since _s 1.0
+ */
+function _s_widgets_init() {
+	register_sidebar( array(
+		'name' => __( 'Sidebar', '_s' ),
+		'id' => 'sidebar-1',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget' => '</aside>',
+		'before_title' => '<h1 class="widget-title">',
+		'after_title' => '</h1>',
+	) );
+}
+add_action( 'widgets_init', '_s_widgets_init' );
+
+/**
+ * Enqueue scripts and styles
+ */
+function _s_scripts() {
+	wp_enqueue_style( 'style', get_stylesheet_uri() );
+
+	wp_enqueue_script( 'small-menu', get_template_directory_uri() . '/js/small-menu.js', array( 'jquery' ), '20120206', true );
+
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
+
+	if ( is_singular() && wp_attachment_is_image() ) {
+		wp_enqueue_script( 'keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
+	}
+}
+add_action( 'wp_enqueue_scripts', '_s_scripts' );
+
+/**
+ * Implement the Custom Header feature
+ */
+//require( get_template_directory() . '/inc/custom-header.php' );
