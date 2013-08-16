c525e38d1855e5a106204c89ef373b719fbdb0f6
Hendra <hendra2392@gmail.com>
2013-08-16 20:39:43 +0700

    removed archives from default widgets

--- sidebar.php
+++ sidebar.php
@@ -9,13 +9,6 @@
 		<div id="secondary" class="widget-area" role="complementary">
 			<?php do_action( 'before_sidebar' ); ?>
 			<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
-				<aside id="archives" class="widget">
-					<h1 class="widget-title"><?php _e( 'Archives', 'mog' ); ?></h1>
-					<ul>
-						<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
-					</ul>
-				</aside>
-
 				<aside id="meta" class="widget">
 					<h1 class="widget-title"><?php _e( 'Meta', 'mog' ); ?></h1>
 					<ul>
